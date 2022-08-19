d1156af813be3e81480050bf5146306662100356
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2022-08-19 16:40:28 +0100

    CORS

--- routes/tl_payment.js
+++ routes/tl_payment.js
@@ -4,11 +4,15 @@ import { getAccessToken,
          storeTransaction,
          getPaymentStatus,
         } from '../controllers/tl_payment.js'
-
+import cors from 'cors'
 const router = express.Router()
 
+const corsOptions = {
+        origin: "http://localhost:3000"
+}
+
 router.post('/getAccessToken', getAccessToken)
-router.post('/initiate/:accessToken', initiatePayment)
+router.post('/initiate/:accessToken', cors(corsOptions), initiatePayment)
 router.post('/storeTransaction/:transactionID/:merchantID/:accountID/:rvnuCodeID/:currency/:amount', storeTransaction)
 router.get('/status/:paymentId', getPaymentStatus)
 
